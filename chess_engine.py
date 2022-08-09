1bc806cc549b61cea93546dafd306e0724c8bc22
Ryan Vackner <105239778+ryantvackner@users.noreply.github.com>
2022-08-08 22:57:21 -0400

    created move class

--- chess_engine.py
+++ chess_engine.py
@@ -27,3 +27,44 @@ class GameState():
         
         # move log (PGN)
         self.move_log = []
+        
+    def make_move(self, move):
+        # find the starting square using the move notation input
+        # make it blank - 
+        self.board[7][5] = "-"
+        
+        # set piece moved to correct location
+        self.board[5][5] = "P"
+        
+        # add move to move log using proper chess notation
+        self.move_log.append(move)
+        
+        # swap whos turn it is to move
+        self.white_to_move = not self.white_to_move
+        pass
+    
+
+class Move():
+    
+    # maps keys to vallues
+    # key : value
+    ranks_to_rows = {"1" : 8, "2" : 7, "3" : 6, "4" : 5,
+                     "5" : 4, "6" : 3, "7" : 2, "8" : 1}
+    rows_to_ranks = {v : k for k, v in ranks_to_rows.items()}
+
+    files_to_cols = {"a" : 1, "b" : 2, "c" : 3, "d" : 4,
+                     "e" : 5, "f" : 6, "g" : 7, "h" : 8}
+    cols_to_files = {v : k for k, v in files_to_cols.items()}
+    
+    
+    def __init__(self, move, board):
+        self.move = move
+        
+        
+    def get_chess_notation(self):
+        pass
+        
+        
+    def get_rank_file(self, r, c):
+        return self.cols_to_files[c] + self.rows_to_ranks[r]
+    
