03bd254172acaa06aa9d3c6ba0a57dfbfacd2c9d
Ryan Vackner <105239778+ryantvackner@users.noreply.github.com>
2022-08-10 22:58:51 -0400

    check pgn instead of move

--- chess_main.py
+++ chess_main.py
@@ -36,8 +36,10 @@ def main():
             move_id = valid_moves[0].index(move)
             piece = valid_moves[1][move_id]
             start_sq = valid_moves[2][move_id]
+            rank_file_move = valid_moves[3][move_id]
+            
             # make the move
-            gs.make_move(move, piece, start_sq)
+            gs.make_move(rank_file_move, piece, start_sq)
             
             # flag that the move has been made
             move_made = True
