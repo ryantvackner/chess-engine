fbc72b5c236e32b7320e8179582d2d08cfad2d09
Ryan Vackner <105239778+ryantvackner@users.noreply.github.com>
2022-09-30 15:41:22 -0400

    Allowed for turns for black and white

--- chess_main.py
+++ chess_main.py
@@ -12,44 +12,82 @@ def main():
     # create the board/gamestate
     gs = chess_engine.GameState()
     
-    # create the valid moves
-    valid_moves = gs.get_valid_moves()
-    print(valid_moves)
-    
-    # flag var to indicate a move has been made
-    move_made = False
-    
-    # print the board
-    draw_board(gs.board)
-    
     # game is running
     running = True
     
-    # first move
-    if running:
-        # get move 1 input
-        move = input("1. ")
-        
-        # check if the move is valid
-        if move in valid_moves[0]:
-            # find the move id and find out what piece made the move
-            move_id = valid_moves[0].index(move)
-            piece = valid_moves[1][move_id]
-            start_sq = valid_moves[2][move_id]
-            rank_file_move = valid_moves[3][move_id]
-            
-            # make the move
-            gs.make_move(rank_file_move, piece, start_sq)
-            
-            # flag that the move has been made
-            move_made = True
+    # move number
+    move_number = 1
     
-        
+    # first move
+    while running:
+        # print the board
         draw_board(gs.board)
         
-    if move_made:
-        valid_moves = gs.get_valid_moves()
-        move_made = False
+        # white to move
+        while gs.white_to_move:
+            # create the valid moves
+            valid_moves = gs.get_valid_moves()
+        
+            # print valid moves
+            print(valid_moves[0])
+        
+            # get move 1 input
+            move = input(format(move_number) + ". ")
+        
+            # resign the game 
+            if move == "qq":
+                running = False
+                break
+        
+            # check if the move is valid
+            if move in valid_moves[0]:
+                # find the move id and find out what piece made the move
+                move_id = valid_moves[0].index(move)
+                piece = valid_moves[1][move_id]
+                start_sq = valid_moves[2][move_id]
+                rank_file_move = valid_moves[3][move_id]
+                
+                # make the move
+                gs.make_move(rank_file_move, piece, start_sq)
+                
+                # blacks turn now
+                gs.white_to_move = False
+        
+        # redraw the board for black
+        draw_board(gs.board)
+        
+        # black to move
+        while not gs.white_to_move:
+            # create the valid moves
+            valid_moves = gs.get_valid_moves()
+        
+            # print valid moves
+            print(valid_moves[0])
+        
+            # get move 1 input
+            move = input(format(move_number) + ". ")
+        
+            # resign the game 
+            if move == "qq":
+                running = False
+                break
+        
+            # check if the move is valid
+            if move in valid_moves[0]:
+                # find the move id and find out what piece made the move
+                move_id = valid_moves[0].index(move)
+                piece = valid_moves[1][move_id]
+                start_sq = valid_moves[2][move_id]
+                rank_file_move = valid_moves[3][move_id]
+                
+                # make the move
+                gs.make_move(rank_file_move, piece, start_sq)
+                
+                # blacks turn now
+                gs.white_to_move = True
+        
+        # increase the move number
+        move_number += 1
         
 
 def draw_board(board):
